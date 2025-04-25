670b03df59ec5a7b506a95e061ae8de123c50986
Larpingink <ahf3ingfabwg@gmail.com>
2025-04-25 18:10:51 +0200

    Delete larping.sql

--- larping.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pastes (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  title TEXT,
-  content TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  expires_at TIMESTAMP,
-  is_public BOOLEAN DEFAULT TRUE,
-  delete_token UUID DEFAULT gen_random_uuid()
-);
